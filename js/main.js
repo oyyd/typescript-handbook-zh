a6c786f0ee9e2bbd5ca3b92ebdf90250d02d33b2
oyyd <oyydoibh@gmail.com>
2015-05-07 23:52:25 +0800

    clear cache

--- js/main.js
+++ js/main.js
@@ -14,13 +14,13 @@
     $nav = $sideBar.find('[role="side-bar-nav"]');
 
   var getFiles = function(cb) {
-    $.get(prefix + 'dest/content.html').done(function(html){
+    $.get(prefix + 'dest/content.html?20150507').done(function(html) {
       var $file = $('<div role="content" class="content"></div>');
       $file.html(html);
       $files.append($file);
       bindEvents($file);
       cb && cb();
-    }).fail(function(){
+    }).fail(function() {
       console.err('failed to load content');
     });
   };
@@ -28,72 +28,72 @@
   var bindEvents = function($e) {
     $e.find('[role="check-ori"]').click(function() {
       var $p = $(this).parents('div[role="item"]').find('p[role="ori"]');
-      $p.is(':visible')?$p.fadeOut():$p.fadeIn();
+      $p.is(':visible') ? $p.fadeOut() : $p.fadeIn();
     });
   };
-   
-  var initHighlight = function(){
-    $files.find('pre code').each(function(i, block){
+
+  var initHighlight = function() {
+    $files.find('pre code').each(function(i, block) {
       hljs.highlightBlock(block);
-    });  
+    });
   };
 
-  var initSideNav = function(){
+  var initSideNav = function() {
     var $titles = $files.find('.section-title');
     var navItems = [];
     var section = null;
-    
-    $titles.each(function(){      
+
+    $titles.each(function() {
       var title = this;
-      console.log(title);      
-      var nodeName = title.nodeName.toLowerCase(); 
-      if(nodeName === 'h1'){
-        if(section){
+      console.log(title);
+      var nodeName = title.nodeName.toLowerCase();
+      if (nodeName === 'h1') {
+        if (section) {
           navItems.push(section);
         }
         section = {
-          title : $(title).text(),
-          subTitles : []          
-        };      
-      } else if(nodeName === 'h2'){
+          title: $(title).text(),
+          subTitles: []
+        };
+      } else if (nodeName === 'h2') {
         section.subTitles.push($(title).text());
       }
     });
-    if(section){
+    if (section) {
       navItems.push(section);
     }
-    
-    for(var index in navItems){
+
+    for (var index in navItems) {
       var section = navItems[index];
       var $section = $('<div role="item" class="item"></div>');
-      $section.append($('<h3><a href="#'+section.title+'">'+section.title+'</a></h3>'));
+      $section.append($('<h3><a href="#' + section.title + '">' + section.title + '</a></h3>'));
       var $list = $('<div role="list" class="list"></div>');
       $section.append($list);
-      for(var index in section.subTitles){
+      for (var index in section.subTitles) {
         var subTitle = section.subTitles[index];
-        $list.append($('<h5><a href="#'+subTitle+'">'+subTitle+'</a></h5>'));        
+        $list.append($('<h5><a href="#' + subTitle + '">' + subTitle + '</a></h5>'));
       }
       $nav.append($section);
     }
-    
+
     var $items = $nav.find('[role="item"]');
-    $items.click(function(){
+    $items.click(function() {
       $items.removeClass('active');
       $(this).addClass('active');
     });
-    
-    $(window).scroll(function(){
-      if($(document).scrollTop()>83){
+
+    $(window).scroll(function() {
+      if ($(document).scrollTop() > 83) {
         $sideBar.addClass('scroll');
-      }else{
+      } else {
         $sideBar.removeClass('scroll');
       }
     });
   };
-    
-  var init = function() {    
-    getFiles(function(){
-      initHighlight();      
+
+  var init = function() {
+    getFiles(function() {
+      initHighlight();
       initSideNav();
     });
   };
